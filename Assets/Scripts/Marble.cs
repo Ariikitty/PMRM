fab21d5d080ef4b1642c5db6b7d6bd676a0fe508
Steveycat <45834971+Steveycat@users.noreply.github.com>
2021-05-06 16:48:09 +0100

    Added Marbles

---
+++ Assets/Scripts/Marble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marble : MonoBehaviour 
+{
+    public GameObject marble;
+    public AudioSource marbleImpactSound;
+    public Material marbleInside;
+
+    private void Start()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 2)
+        {
+            marbleImpactSound.Play();
+        }
+    }
+}
